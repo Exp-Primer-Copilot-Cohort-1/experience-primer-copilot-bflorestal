9d0f2c0415306d5842e83bb0c21bc98ec3438473
Bryan Florestal <61915780+bflorestal@users.noreply.github.com>
2024-02-25 20:07:40 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,27 @@
+// Create web server
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const { pathname } = url.parse(req.url);
+    const filePath = path.join(__dirname, pathname);
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    }
+    );
+});
+
+server.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+});
